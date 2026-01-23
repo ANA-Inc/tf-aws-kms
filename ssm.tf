9b48c73538b59fa58b71da84c8a244b20262e3fe
pramod-ana <palluri@anacorp.com>
2026-01-23 14:32:56 -0800

    Update ssm.tf

--- ssm.tf
+++ ssm.tf
@@ -1,5 +1,5 @@
 module "ssm" {
-  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-ssm-parameter.git?ref=ff3a868a2f88b9540691daa87670b4422abf0bec"
+  source = "git::https://github.com/ANA-Inc/terraform-aws-ssm-parameter.git?ref=ff3a868a2f88b9540691daa87670b4422abf0bec"
 
   name  = coalesce(var.ssm_parameter_name, "/kms/${var.env}/${local.name}")
   value = module.kms.key_id
